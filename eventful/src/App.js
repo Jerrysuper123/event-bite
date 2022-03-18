b0f9a970b53de0aaa2ec34e2e319f20fce1f2196
Jerry Chen <jerryq.chen@gmail.com>
2022-03-19 07:56:09 +0800

    added some changes to app.js

--- eventful/src/App.js
+++ eventful/src/App.js
@@ -65,6 +65,7 @@ class App extends React.Component {
   };
 
   componentDidMount() {
+    // able to get user location
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log("Latitude is :", position.coords.latitude);
       console.log("Longitude is :", position.coords.longitude);
