d6f455d41e5843333452846b91ae45cf1bda86f9
Jerry Chen <jerryq.chen@gmail.com>
2022-03-31 08:43:58 +0800

    added filter result summary bar to filter bar

--- eventful/src/FilterBar/FilterBar.js
+++ eventful/src/FilterBar/FilterBar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BASE_API_URL } from "../Utility";
 import axios from "axios";
 import filter from "../images/filter.png";
+import clearFilter from "../images/clearFilter.png";
 
 import "./style.css";
 
@@ -12,6 +13,8 @@ export default class FilterBar extends React.Component {
     dateTimeFilter: "",
     searchString: "",
     searchEventStartDate: "",
+    searchCategories: [],
+    searchHashtags: [],
   };
 
   componentDidMount = async () => {
@@ -45,124 +48,227 @@ export default class FilterBar extends React.Component {
     this.props.searchByDate(this.state.searchEventStartDate);
   };
 
+  processCheckbox = (e) => {
+    let currentValues = this.state[e.target.name];
+    let modifiedValues;
+
+    if (!currentValues.includes(e.target.value)) {
+      modifiedValues = [...currentValues, e.target.value];
+    } else {
+      modifiedValues = currentValues.filter((element) => {
+        return element !== e.target.value;
+      });
+    }
+
+    this.setState({
+      [e.target.name]: modifiedValues,
+    });
+  };
+
   render() {
     return (
-      <nav
-        id="filterBar"
-        className="navbar navbar-expand-lg navbar-light bg-light shadow"
-      >
-        <div className="container-fluid">
-          <button
-            className="filterToggler navbar-toggler ms-auto"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarFilter"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span
-              style={{
-                fontSize: "0.9rem",
-              }}
+      <React.Fragment>
+        <nav
+          id="filterBar"
+          className="navbar navbar-expand-lg navbar-light bg-light shadow"
+        >
+          <div className="container-fluid">
+            <button
+              className="filterToggler navbar-toggler ms-auto"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarFilter"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              filter
-            </span>
-            <img src={filter} alt="filter" style={{ width: "1rem" }} />
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarFilter">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item dropdown ms-lg-3 p-3">
-                <a
-                  className="nav-link dropdown-toggle "
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  category
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  {this.state.filterCategories.map((cat, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            {cat}
-                          </a>
-                        </li>
-                      </React.Fragment>
-                    );
-                  })}
-                </ul>
-              </li>
-
-              <li className="nav-item dropdown p-3">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  tags
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  {this.state.filterTags.map((tag, index) => {
-                    return (
-                      <React.Fragment key={index}>
-                        <li>
-                          <a className="dropdown-item" href="#">
-                            {tag}
-                          </a>
-                        </li>
-                      </React.Fragment>
-                    );
-                  })}
-                </ul>
-              </li>
-            </ul>
-            <div className="d-flex p-3">
-              <label>From date:</label>
-              <input
-                className="form-control me-2"
-                type="date"
-                aria-label="Search"
-                name="searchEventStartDate"
-                value={this.state.searchEventStartDate}
-                onChange={this.updateFormFieldAndSearchByDate}
-                // onChange={() => {
-                //   this.props.searchByDate(this.state.searchEventStartDate);
-                // }}
-              />
-            </div>
-
-            <div className="d-flex p-3">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-                name="searchString"
-                value={this.state.searchString}
-                onChange={this.updateFormField}
-              />
-              <button
-                className="btn btn-outline-success me-3"
-                type="submit"
-                onClick={() => {
-                  this.props.searchEvent(this.state.searchString);
+              <span
+                style={{
+                  fontSize: "0.9rem",
                 }}
               >
-                Search
-              </button>
+                filter
+              </span>
+              <img src={filter} alt="filter" style={{ width: "1rem" }} />
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarFilter">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item dropdown ms-lg-3 p-3">
+                  <a
+                    className="nav-link dropdown-toggle "
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    category
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    {this.state.filterCategories.map((cat, index) => {
+                      return (
+                        <React.Fragment key={index}>
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              <input
+                                type="checkbox"
+                                name="searchCategories"
+                                value={cat}
+                                checked={this.state.searchCategories.includes(
+                                  cat
+                                )}
+                                onChange={this.processCheckbox}
+                              />
+                              <label>{cat}</label>
+                            </a>
+                          </li>
+                        </React.Fragment>
+                      );
+                    })}
+                  </ul>
+                </li>
+
+                <li className="nav-item dropdown p-3">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    tags
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    {this.state.filterTags.map((tag, index) => {
+                      return (
+                        <React.Fragment key={index}>
+                          <li>
+                            <a className="dropdown-item" href="#">
+                              <input
+                                type="checkbox"
+                                name="searchHashtags"
+                                value={tag}
+                                checked={this.state.searchHashtags.includes(
+                                  tag
+                                )}
+                                onChange={this.processCheckbox}
+                              />
+                              <label>{tag}</label>
+                            </a>
+                          </li>
+                        </React.Fragment>
+                      );
+                    })}
+                  </ul>
+                </li>
+
+                <li className="nav-item dropdown p-3">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    date
+                  </a>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <div className="d-flex p-3">
+                      <label>From date:</label>
+                      <input
+                        className="form-control me-2"
+                        type="date"
+                        aria-label="Search"
+                        name="searchEventStartDate"
+                        value={this.state.searchEventStartDate}
+                        onChange={this.updateFormFieldAndSearchByDate}
+                      />
+                    </div>
+                  </ul>
+                </li>
+              </ul>
+
+              <section className="d-flex p-3">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  name="searchString"
+                  value={this.state.searchString}
+                  onChange={this.updateFormField}
+                />
+                <button
+                  className="btn btn-outline-success me-3"
+                  type="submit"
+                  onClick={() => {
+                    this.props.searchEvent(this.state.searchString);
+                  }}
+                >
+                  Search
+                </button>
+              </section>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+        {/* filtered results summary */}
+        <section
+          className="
+        filteredResultSummary
+        d-flex justify-content-around align-items-center"
+        >
+          <span className="d-flex flex-column flex-lg-row">
+            <span className="me-5">Found:</span>
+            <span className="d-flex flex-column flex-lg-row">
+              <span className="me-5">map (0 results)</span>
+              <span>calendar (4 results)</span>
+            </span>
+          </span>
+
+          {/* sort and clear filter button */}
+          <span className="d-flex align-items-center flex-column flex-lg-row">
+            <span className="nav-item dropdown">
+              <span
+                className="nav-link dropdown-toggle sortBtn"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                sort
+              </span>
+              {/* this only shows up on map */}
+              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li>title</li>
+                <li>date</li>
+              </ul>
+            </span>
+
+            <span>
+              clear filters{" "}
+              <img
+                style={{
+                  width: "1rem",
+                }}
+                src={clearFilter}
+                alt="clearFilter"
+              />
+            </span>
+          </span>
+        </section>
+      </React.Fragment>
     );
   }
 }
